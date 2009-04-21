02f2620131abf4c11a4a0572416bf8a8bc904e82
Diego Che <chekenan@gmail.com>
2009-04-22 05:09:57 +0800

    can show their names

---
+++ OurChildren.rb
@@ -0,0 +1,27 @@
+#
+# coryright (c) 2009 Diego Che
+#
+# For our children
+
+class Student
+  attr_accessor :name, :gender, :age, :birthday, :school, :class
+  attr_accessor :city, :address, :parents, :parents_phone
+  
+  def initialize(str)
+    
+  end
+  
+private
+  def parse_name(str)
+  end
+  
+  def parse_gender(str)
+  end
+  
+  def parse_age(str)
+  end
+  
+  def parse_birthday(str)
+  end
+end
+
