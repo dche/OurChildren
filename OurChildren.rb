0d26f1a84a8013d1c4635ec9326386fc1e2941fb
Diego Che <chekenan@gmail.com>
2009-04-26 03:26:35 +0800

    appearence is done

--- OurChildren.rb
+++ OurChildren.rb
@@ -3,25 +3,53 @@
 #
 # For our children
 
-class Student
-  attr_accessor :name, :gender, :age, :birthday, :school, :class
-  attr_accessor :city, :address, :parents, :parents_phone
+require 'rubygems'
+require 'json/pure'
+
+class Child
+  
+  @@attr = [:name, :gender, :birth_year, :birth_month, :birth_day, :age
+      :province, :region, :city, :town, :village, :school, :grade, :class,
+      :rest_address, :father, :mother, :other_relatives, :phone
+    ];
+  
+  @@attr.each do |sym|
+    attr_accessor sym
+  end
+  
+  def self.parser_for(sym, &proc)
+    
+  end
   
   def initialize(str)
     
   end
   
-private
-  def parse_name(str)
+  def is_same_child?(c)
   end
-  
-  def parse_gender(str)
-  end
-  
-  def parse_age(str)
-  end
-  
-  def parse_birthday(str)
+   
+  def to_json(*a)
+    h = {}
+    @@attr.each do |sym|
+      h[sym] = self.send(sym) if self.send(sym)
+    end
+    h.to_json(*a)
   end
 end
 
+# register parsers
+
+# parse names.
+children = [];
+Dir.glob("../student_victims/*.txt") do |f|
+  
+end
+
+# export. group 500 names into on json file.
+0.upto(children.length / 500) do |i|
+  max = i * 500 + 499
+  json = children[(i * 500) .. (max >= children.length ? children.length - 1 : max)].to_json
+  File.open("OurChildren#{i}.json", "w") do |f|
+    f.puts json
+  end
+end
